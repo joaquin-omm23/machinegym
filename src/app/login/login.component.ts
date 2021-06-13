d6b8f66fb0246400362278d37b7f09dd56e0db67
joaquinmoreno <joaquin.omm23@gmail.com>
2021-06-13 13:21:05 -0500

    login validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   formularioLogin: FormGroup;
+  datosCorrectos: boolean = true;
+  textoError: string = '';
   constructor(
     private creadorFormulario: FormBuilder,
     public auth: AngularFireAuth
@@ -22,11 +24,23 @@ export class LoginComponent implements OnInit {
   }
 
   ingresar() {
-    this.auth.signInWithEmailAndPassword(
-      this.formularioLogin.value.email,
-      this.formularioLogin.value.password
-    ).then((usuario)=>{
-      console.log(usuario)
-    });
+    if (this.formularioLogin.valid) {
+      this.datosCorrectos = true;
+      this.auth
+        .signInWithEmailAndPassword(
+          this.formularioLogin.value.email,
+          this.formularioLogin.value.password
+        )
+        .then((usuario) => {
+          console.log(usuario);
+        })
+        .catch((error) => {
+          this.datosCorrectos = false;
+          this.textoError = error.message;
+        });
+    } else {
+      this.datosCorrectos = false;
+      this.textoError = 'Por favor revisa que los datos esten correctos';
+    }
   }
 }
