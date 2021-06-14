c0777488b7bedf84d51dd2e3c861e5d6901d45dc
joaquinmoreno <joaquin.omm23@gmail.com>
2021-06-14 12:00:03 -0500

    avance listado de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,18 @@ import { AngularFireModule } from '@angular/fire';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    ListadoClientesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +26,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     AccordionModule.forRoot(),
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    FormsModule,
     NgxSpinnerModule,
     BsDropdownModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase)
