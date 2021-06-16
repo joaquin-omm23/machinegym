782529563a3791d1875f05a806f7deca3cd8f90e
joaquinmoreno <joaquin.omm23@gmail.com>
2021-06-16 11:53:07 -0500

    registrar clientes

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,30 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cliente',
   templateUrl: './agregar-cliente.component.html',
-  styleUrls: ['./agregar-cliente.component.scss']
+  styleUrls: ['./agregar-cliente.component.scss'],
 })
 export class AgregarClienteComponent implements OnInit {
-
   formularioCliente: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  porcentajeSubida: number = 0;
+  urlImagen: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private storage: AngularFireStorage,
+    private col: AngularFirestore
+  ) {}
 
   ngOnInit() {
     this.formularioCliente = this.fb.group({
       nombre: ['', Validators.required],
-      apellido: ['',Validators.required],
+      apellido: ['', Validators.required],
       correo: ['', Validators.compose([Validators.required, Validators.email])],
       cedula: [''],
       fechaNacimiento: ['', Validators.required],
       telefono: [''],
-      imgUrl: ['', Validators.required]
-    })
+      imgUrl: ['', Validators.required],
+    });
   }
 
-  agregar(){
-    console.log(this.formularioCliente.value)
+  //agregando cliente
+  agregar() {
+    this.formularioCliente.value.imgUrl = this.urlImagen;
+    this.formularioCliente.value.fechaNacimiento = new Date(
+      this.formularioCliente.value.fechaNacimiento
+    );
+    console.log(this.formularioCliente.value);
+    this.col
+      .collection('clientes')
+      .add(this.formularioCliente.value)
+      .then((termino) => {
+        console.log('Registro creado');
+      });
   }
 
+  // subir foto de perfil
+  subirImagen(evento: any) {
+    if (evento.target.files.length > 0) {
+      let nombre = new Date().getTime().toString();
+      let archivo = evento.target.files[0];
+
+      let extension = archivo.name
+        .toString()
+        .substring(archivo.name.toString().lastIndexOf('.'));
+
+      let ruta = 'clientes/' + nombre + extension;
+      const referencia = this.storage.ref(ruta);
+      const tarea = referencia.put(archivo);
+      tarea.then((objeto) => {
+        referencia.getDownloadURL().subscribe((url) => {
+          this.urlImagen = url;
+        });
+      });
+
+      tarea.percentageChanges().subscribe((porcentaje) => {
+        this.porcentajeSubida = parseInt(porcentaje.toString());
+      });
+    }
+  }
 }
