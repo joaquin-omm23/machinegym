2c0397fbdf608c8d703902459d77846aef6ed83d
MiguelRamirezOsorio <miguel.ramirez6@unmsm.edu.pe>
2021-06-19 22:02:30 -0500

    Agregado mostrar clientes inscripcion

--- src/app/seleccionar-cliente/seleccionar-cliente.component.ts
+++ src/app/seleccionar-cliente/seleccionar-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Cliente } from '../models/cliente';
 
 @Component({
   selector: 'app-seleccionar-cliente',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seleccionar-cliente.component.scss']
 })
 export class SeleccionarClienteComponent implements OnInit {
+  clientes: Cliente[] = new Array<Cliente>();
+  constructor(private db: AngularFirestore) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.db.collection<any>('clientes').get().subscribe((resultados)=>{
+      this.clientes.length = 0
+      resultados.docs.forEach((item)=>{
+        let cliente: Cliente = item.data();
+        cliente.id = item.id;
+        cliente.ref = item.ref;
+        cliente.visible = false;
+        this.clientes.push(cliente);
+      })
+    })
   }
+  
+  buscarClientes(nombre: string){
+    console.log(nombre)
+  }
+
 
 }
